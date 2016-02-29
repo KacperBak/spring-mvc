4965e9b7292676d38329f888623d16caad24d2f5
Kacper Bak <github@kacperbak.de>
2016-02-29 22:25:09 +0100

    dev and prod data sources configured with profiles

--- src/main/java/de/kacperbak/config/DataConfig.java
+++ src/main/java/de/kacperbak/config/DataConfig.java
@@ -2,14 +2,12 @@ package de.kacperbak.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.jndi.JndiObjectFactoryBean;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -18,37 +16,31 @@ import javax.sql.DataSource;
 @Configuration
 public class DataConfig {
 
-//    @Bean
-//    public DataSource dataSource(){
-//        return new EmbeddedDatabaseBuilder()
-//                .setType(EmbeddedDatabaseType.H2)
-//                .addScript("h2-schema.sql")
-//                .build();
-//    }
+    @Bean
+    @Profile("dev")
+    public DataSource developmentDataSource(){
+        //create data source
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/vwdev");
+        dataSource.setUsername("de.kacperbak.dev");
+        dataSource.setPassword("asdf");
 
-//    @Bean
-//    public DataSource dataSource(){
-//        //create data source
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("org.postgresql.Driver");
-//        dataSource.setUrl("jdbc:postgresql://localhost:5432/vwdev");
-//        dataSource.setUsername("de.kacperbak.dev");
-//        dataSource.setPassword("asdf");
-//
-//        //populate data source
-//        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-//        populator.addScripts(new ClassPathResource("postgresql-schema.sql"));
-//        populator.setSeparator(";");
-//        try{
-//            populator.populate(dataSource.getConnection());
-//        } catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        return dataSource;
-//    }
+        //populate data source
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScripts(new ClassPathResource("postgresql-schema.sql"));
+        populator.setSeparator(";");
+        try{
+            populator.populate(dataSource.getConnection());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
 
     @Bean
-    public DataSource dataSource(){
+    @Profile("prod")
+    public DataSource productionDataSource(){
         DataSource dataSource = null;
         try{
             Context ctx = new InitialContext();
--- src/main/resources/h2-schema.sql
+++
@@ -1,7 +0,0 @@
-create table Word (
-	id identity,
-  language varchar(255) not null,
-	characters varchar(255) not null
-);
-
-INSERT INTO Word(id, language, characters) VALUES (101, 'EN', 'BUS');
